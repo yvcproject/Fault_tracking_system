37da5f91847696a24bdfd1170586024de1aa24f6
AdiHemo <62175021+AdiHemo@users.noreply.github.com>
2020-08-20 19:31:35 +0300

    Update Register.php

--- FaultSystemProject/Register.php
+++ FaultSystemProject/Register.php
@@ -29,9 +29,9 @@
 	 <span style="font-size:30px;cursor:pointer" dir="ltr" onclick="openNav()">&#9776; <?php
 echo 'Hello, ' . htmlspecialchars($_COOKIE["FirstName"]) ;
 ?> </span>
-  <a class="navbar-brand" href="http://localhost/FaultSystemProject/mainPage.php" style="font-size:30px;cursor:>
-    <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-    FaultSystemProject - YAR
+  <a class="navbar-brand" href="http://localhost/FaultSystemProject/mainPage.php" style="font-size:30px;cursor:">
+      FaultSystemProject - YAR
+    <img src="YARLOGO.jpg" alt="logo" width="80" height="40" class="d-inline-block align-top" loading="lazy">
   </a>
 
 </nav>
