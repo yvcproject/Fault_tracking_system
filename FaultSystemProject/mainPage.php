e1af89ed1e7e8984d3b63d7c0ef4bc125a6756d5
AdiHemo <62175021+AdiHemo@users.noreply.github.com>
2020-08-29 15:07:12 +0300

    Update mainPage.php

--- FaultSystemProject/mainPage.php
+++ FaultSystemProject/mainPage.php
@@ -50,7 +50,8 @@ $conn = mysqli_connect($server,$username,$password,$dbname);
  ?>
 
 <div class="mainContainer back">
-
+<h1>Fault System</h1>
+<br>
 <!-- Filter and Button -->
       <div class="dropdown">
       <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -76,6 +77,7 @@ $conn = mysqli_connect($server,$username,$password,$dbname);
 
 
 <div style="padding-left: 10%; padding-right: 10%; align-content: center">
+
  
 <!-- Fault Table From DB -->
 <!--<body style="padding-left: 10%; padding-right: 10%; align-content: center"> --> 
