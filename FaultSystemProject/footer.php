6169b5eb9646912b1c09fb15fec68b3f197c67ec
AdiHemo <62175021+AdiHemo@users.noreply.github.com>
2020-09-05 12:52:08 +0300

    Update footer.php

--- FaultSystemProject/footer.php
+++ FaultSystemProject/footer.php
@@ -1,15 +1,17 @@
 <?php
 echo '
-	<div class="footer" id="footer" style="
-                                              left: 0;
-                                              bottom: 0;
-                                              padding: 0.1em;
-                                              background-color: #778899;
-                                              color: white;
-                                              text-align: center;
-                                              font-weight: bold;"
+    <div class="footer" id="footer" style="
+                                            position: absolute;
+                                            width: 100%;
+                                            left: 0;
+                                            bottom: 0;
+                                            padding: 0.1em;
+                                            background-color: #778899;
+                                            color: white;
+                                            text-align: center;
+                                            font-weight: bold;"
 
-	                                            >
+	                                        >
 		<p>&copy; YAR 2020</p>
 	</div>
 ';
