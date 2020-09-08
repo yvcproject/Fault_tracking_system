e6a133cff85cc11198345605b565fbf645cbcf73
AdiHemo <62175021+AdiHemo@users.noreply.github.com>
2020-09-08 19:55:54 +0300

    Update index.php

--- FaultSystemProject/index.php
+++ FaultSystemProject/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Sign in</title>
+    <title>Log in</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
     <link rel="stylesheet" type="text/css" href="general.css">
